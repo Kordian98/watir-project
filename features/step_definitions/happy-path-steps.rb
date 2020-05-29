55fb39ae97c75dff18749f3c6a20257b8202fe9c
kordian98 <kordian.kicza@gmail.com>
2020-05-29 19:41:00 +0200

    Structure change, cucumber implementation, first feature and step definition

--- features/step_definitions/happy-path-steps.rb
+++ features/step_definitions/happy-path-steps.rb
@@ -1,6 +1,6 @@
 require_relative '../pages/joto_form_page.rb'
 
 Given(/^a user goes to Joto Form Homepage$/) do
-  @amazon_page = SearchPage.new(@browser)
-  @amazon_page.visit_amazon_site
+  @joto_form_page = JotoFormPage.new(@browser)
+  @joto_form_page.get_joto_page
 end
